7d85563aa3ade312df2bd1f0c3e6e3cf8a7631c9
Frivridna <frivridnadagar@gmail.com>
2021-09-12 00:48:48 +0200

    remove lines in app.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,5 @@
 const app = require('./app');
 
-const products = './data/products.json'
-
-
-// Route to return all products
-app.get('/sneakers', (req, res) => {
-  res.json({ data: products })
-})
-
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   /* eslint-disable no-console */
